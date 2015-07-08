3d0f775670c5f256025a225a9eea3bf6db79d0e9
Andrew Rasch <raschykid@gmail.com>
2015-07-08 18:21:27 -0400

    Implementing kernel density differences

--- cv/test_CV.py
+++ cv/test_CV.py
@@ -2,26 +2,27 @@ import pprint
 
 from Authenticator import Authenticator
 from CV import CV
+import Density as dn
 
 class test_auth(Authenticator):
     def train(self, training_data):
-        print 'training', training_data
-    
+        refProf = dn.density(training_data[:len(training_data)//3])
+        trainingProf = dn.density(training-data[len(trainin_data)//3:])
+        dn.difference(refProf,trainingProf)
     def evaluate(self, val_data):
         print 'validating', val_data
         print
 
-
 if __name__=='__main__':
-    test_data = {'a' : [{'aa' : range(10,1000,100), 
+    test_data = {'a' : [{'aa' : range(10,1000,100),
                          'ab' : range(10)},
                         {'ac' : range(0,10,2)},
                         {'ad' : range(10, 20, 3)}
                     ],
-                 'b' : [{'ba' : range(0,20,3), 
+                 'b' : [{'ba' : range(0,20,3),
                          'bb' : range(15)},
-                        {'bc' : range(2,20,4)},
-                        {'bd' : range(50,300,150)}
+                        {'bc' : range(2,20,4),
+                         'bd' : range(50,300,150)}
                     ],
     }
 
